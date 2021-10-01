5b306b958aab5b4807285232b61237c7d528d5df
Yunus ÖLMEZ <yns4832@gmail.com>
2021-10-01 07:12:19 +0200

    some layout experiments, will be decided later

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <h1>SpaceX</h1>
+    </header>
+    <div class="main">
+      <Nuxt />
+    </div>
+    <footer>footer</footer>
+  </div>
+</template>
+
+<style>
+.wrapper {
+  padding: 0 1rem;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+.main {
+  padding: 0.5rem 0;
+}
+</style>
