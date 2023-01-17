f595e066aef13b292b2ec054e5a445325cb984a9
Yunus ÖLMEZ <yns4832@gmail.com>
2023-01-17 15:35:07 +0100

    npx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spoxx.rocket',
+  appName: 'Rocket App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
